d73b33de2006186ff10bead095468fa783ac0db2
Piyush7034 <piyushshukla2100@gmail.com>
2025-01-17 13:52:25 +0530

    Some changes
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- soap-dataprovider-plugin/src/main/java/io/mosip/certify/soapdataprovider/integration/service/SoapDataProviderPlugin.java
+++ soap-dataprovider-plugin/src/main/java/io/mosip/certify/soapdataprovider/integration/service/SoapDataProviderPlugin.java
@@ -2,8 +2,7 @@ package io.mosip.certify.soapdataprovider.integration.service;
 
 import io.mosip.certify.api.exception.DataProviderExchangeException;
 import io.mosip.certify.api.spi.DataProviderPlugin;
-import io.mosip.certify.wsdl.ListOfCountryNamesByNameResponse;
-import io.mosip.certify.wsdl.TCountryCodeAndName;
+import io.mosip.certify.wsdl.NumberToDollarsResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 @ConditionalOnProperty(value = "mosip.certify.integration.data-provider-plugin", havingValue = "SoapDataProviderPlugin")
@@ -18,17 +18,16 @@ import java.util.Map;
 @Slf4j
 public class SoapDataProviderPlugin implements DataProviderPlugin {
     @Autowired
-    CountryClient countryClient;
+    NumberConversionClient numberConversionClient;
 
     @Override
     public JSONObject fetchData(Map<String, Object> identityDetails) throws DataProviderExchangeException {
-        ListOfCountryNamesByNameResponse response = countryClient.getListOfCountriesByName();
-        TCountryCodeAndName tCountryCodeAndName = response.getListOfCountryNamesByNameResult().getTCountryCodeAndName().get(0);
+        String response = numberConversionClient.convert(new BigDecimal(123456.85));
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("name",tCountryCodeAndName.getSName());
-            jsonObject.put("capital", tCountryCodeAndName.getSISOCode());
-            jsonObject.put("currency", tCountryCodeAndName.getSISOCode());
+            jsonObject.put("name", "India");
+            jsonObject.put("capital", "Delhi");
+            jsonObject.put("currency", response);
             jsonObject.put("population", 6567456);
             return jsonObject;
         } catch (JSONException e) {
